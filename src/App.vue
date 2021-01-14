8a98d74e877924309c15f75ac0fa1ad7ecc0529f
sebastian2449 <77408993+sebastian2449@users.noreply.github.com>
2021-01-14 16:30:56 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 
 <style>
 #app{
-    font-family: Avenir, Helvetica, Arial, Helvetica, sans-serif;
+    font-family:  Helvetica, Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -10,7 +10,7 @@
 
 <script>
 export default {
-  name: 'UserProfile',
+  name: "UserProfile",
   
   data(){
     return{

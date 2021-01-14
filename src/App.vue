60fe5992b1c3c6e628907e585be589ef8317b6b0
sebastian2449 <77408993+sebastian2449@users.noreply.github.com>
2021-01-14 15:55:47 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My test page "/>
-  </div>
+    <div id="app">
+        <user-profile/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import UserProfile from "./components/UserProfile";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name:'App',
+    components:{UserProfile}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+    font-family: Avenir, Helvetica, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-height: 100vh;
+    background-color: #F3F5FA;
 }
+
 </style>
+
---
+++ src/UserProfile.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class"user-profile">
+    <div class "user-profile_user-panel"\>
+      <h1 class="user-profile__user-name">@{{user.username}}></h1>
+      <div class="user-profile__follower-count">
+        <strong>Followers:</strong>{{followers}}
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'UserProfile',
+  
+  data(){
+    return{
+      followers:0,
+      user:{
+        id:1,
+        username:'sebastian2449 ',
+        firstName: 'Sebastian',
+        lastName: 'Vanegas Ruiz',
+        email: 'sebastian2449@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount<newFollowerCount){
+        console.log(`${this.user.username} has gain a follower`)
+      }
+    }
+  },
+
+  computed:{
+    fullName(){
+      return   this.user.firstName+' '+this.user.lastName;
+    }
+  },
+
+  methods:{
+
+    followUsers(){
+      this.followers++
+    }
+  },
+
+  mounted(){
+    this.followUsers();
+  }
+
+
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height:100vh;
+  background-color: #F3F5FA;
+
+}
+
+h1{
+  margin:0;
+}
+
+</style>
